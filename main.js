8ff844e1e43d326770338004ff76a338ec131799
truptigaonkar <trups11.trupti@gmail.com>
2019-01-30 12:12:29 +0100

    Fixed bugs and basic styling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let formAddList = document.querySelector('form');
+
 let inputAddList = document.querySelector('input');
 let ulAddList = document.querySelector('#ulAddList');
+let formAddList = document.querySelector('form');
 formAddList.addEventListener('submit', addList);
 
 //Add/Create Event
@@ -14,30 +15,39 @@ function addList(e) {
     if (inputValue === '') {
         alert('You must write something!');
     } else {
-        liAddList.appendChild(document.createTextNode(inputValue));
+        liAddList.appendChild(document.createTextNode(inputValue)); 
         ulAddList.appendChild(liAddList);
     }
 
     // Attaching Add card button
     let btnAddCard = document.createElement('button');
     btnAddCard.className = 'addCard';
-    btnAddCard.appendChild(document.createTextNode('+'));
+    btnAddCard.textContent = "+ Add Card";
+    //btnAddCard.appendChild(document.createTextNode('+'));
     liAddList.appendChild(btnAddCard);
 
     // Attaching Delete button
     let btnDelete = document.createElement('button');
-    btnDelete.className = 'delete';
-    btnDelete.appendChild(document.createTextNode('X'));
+    btnDelete.className = 'deleteList';
+    btnDelete.textContent = "X";
+    //btnDelete.appendChild(document.createTextNode('X'));
     liAddList.appendChild(btnDelete);
 }
 
 //Delete Event
 ulAddList.addEventListener('click', removeList);
-function removeList(e){
-    if (e.target.classList.contains('delete')) {
-    if (confirm('Are you sure want to delete the element?')) {
-        let li = e.target.parentElement;
-        ulAddList.removeChild(li);
+function removeList(e) {
+    if (e.target.classList.contains('deleteList')) {
+        if (confirm('Are you sure want to delete the element?')) {
+            let li = e.target.parentElement;
+            ulAddList.removeChild(li);
+        }
     }
 }
+
+//Add card event
+ulAddList.addEventListener('click', addCard);
+function addCard(e){
+    e.preventDefault();
 }
+
